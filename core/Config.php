7fa543d95a6e7431e8fb019566335f12d78a6644
Jiří Velek <jvelek@students.zcu.cz>
2021-10-02 14:28:28 +0200

    set autoload folders to be general configuration, add routes, remove cookies from _REQUEST

--- core/Config.php
+++ core/Config.php
@@ -1,19 +1,26 @@
 <?php
 
+// we don't want cookies mixed with POST and GET data
+ini_set("request_order", "PG");
+
 $GLOBALS["config"] = [
     "database" => [
         "hostname" => "127.0.0.1",
         "username" => "root",
         "password" => "",
         "port" => 3306
+    ],
+
+    "autoload_folders" => ["controller", "core", "model", "view", "helper"],
+    "routes" => [
+        "/" => "home"
     ]
 ];
 
 spl_autoload_register(function($class) {
     $filename = strtolower($class) . ".php";
-    $folders = ["controller", "core", "model", "view"];
 
-    foreach($folders as $folder) {
+    foreach($GLOBALS["config"]["autoload_folders"] as $folder) {
 
         $path = $folder . "/" . $filename;
 
