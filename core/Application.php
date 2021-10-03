aff912bde5b15b77e35b5cf99e983fa1261d33ef
Jiří Velek <jvelek@students.zcu.cz>
2021-10-03 13:33:09 +0200

    code moved from router to make more sense

--- core/Application.php
+++ core/Application.php
@@ -2,11 +2,35 @@
 
 class Application {
 
-    private $model, $controller, $view, $router;
+    private Model $model;
+    private Controller $controller;
+    private $view;
+    private Router $router;
 
     public function __construct() {
 
         $this->router = new Router();
-        
+
+        $config = $GLOBALS["config"];
+        $route = $this->router->getRoute();
+
+        $modelName = $config["model"][$route];
+        $viewName = $config["view"][$route];
+        $controllerName = $config["controller"][$route];
+
+        // create instance of controller, provide:
+        // model instance
+        // TODO: twig instnance !!
+        // all params from url
+        // 
+        // when controller is done => render everything
+
+        $model = new $modelName();
+
+        // TODO: add twig
+        $twig = null;
+
+        $this->controller = new $controllerName($model, $twig);
+        $this->controller->execute($this->router->getParams());
     }
 }
