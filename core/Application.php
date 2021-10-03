5f6d271829652bef6176c74125da52b244ea608b
Jiří Velek <jvelek@students.zcu.cz>
2021-10-03 12:26:52 +0200

    add router instance

--- core/Application.php
+++ core/Application.php
@@ -2,11 +2,11 @@
 
 class Application {
 
-    private $model, $controller, $view;
+    private $model, $controller, $view, $router;
 
     public function __construct() {
 
-        
+        $this->router = new Router();
         
     }
 }
