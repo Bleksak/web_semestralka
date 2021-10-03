8964a2953394ce9fa0163fcde4e8d4e54b717e13
Jiří Velek <jvelek@students.zcu.cz>
2021-10-03 13:34:16 +0200

    moved code from router to application

--- core/Router.php
+++ core/Router.php
@@ -1,48 +1,60 @@
 <?php
 
-class Router {
+class Router
+{
 
-    public function buildRoute() {
+    private Controller $controller;
+
+    private string $route;
+    private array $params;
+
+    public function createController()
+    {
         $config = $GLOBALS["config"];
         $base = $config["routes"];
         $uri = explode("/", Server::getRoutePath());
         array_shift($uri);
 
-        if(empty($uri)) {
+        if (empty($uri)) {
             $uri = [""];
         }
-        
+
         $route = $base;
         $i = 0;
 
-        while($i < sizeof($uri) && isset($route[$uri[$i]])) {
+        while ($i < sizeof($uri) && isset($route[$uri[$i]])) {
             $route = $route[$uri[$i]];
             $i += 1;
         }
 
-        if($i == 0) {
+        if ($i == 0) {
             $route = $base["404"];
         }
 
-        if(is_array($route)) {
-            if(isset($route[""])) {
+        if (is_array($route)) {
+            if (isset($route[""])) {
                 $route = $route[""];
             } else {
                 $route = $base["404"];
             }
         }
 
-        $params = array_slice($uri, $i);
-
-        $modelName = $config["model"][$route];
-        $viewName = $config["view"][$route];
-        $controllerName = $config["controller"][$route];
-
-        
-
+        $this->route = $route;
+        $this->params = array_slice($uri, $i);
     }
 
-    public function __construct() {
-        $this->buildRoute();
+    public function __construct()
+    {
+        $this->createController();
+    }
+
+    public function getRoute(): string
+    {
+        return $this->route;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
     }
 }
