ccb36f5a2a4343cb64affdbc2b393ed40002e33b
Jiří Velek <jvelek@students.zcu.cz>
2021-10-03 11:48:11 +0200

    add comments, add function to get route path

--- helper/Server.php
+++ helper/Server.php
@@ -1,7 +1,11 @@
 <?php
 
+/**
+ * Server class for managing URLs and files
+ * 
+ * @author Jiri Velek
+ */
 class Server {
-
     private static function get($name) {
         if(!isset($_SERVER[$name])) {
             return "";
@@ -10,15 +14,34 @@ class Server {
         return $_SERVER[$name];
     }
 
-    public static function getURI() {
+    /**
+     * returns request uri
+     */
+    public static function getRequestURI() {
         return self::get("REQUEST_URI");
     }
 
+    /**
+     * returns query string
+     */
     public static function getQueryString() {
         return self::get("QUERY_STRING");
     }
 
+    /**
+     * returns current script directory
+     */
     public static function getScriptDirectory() {
         return dirname(self::get("PHP_SELF"));
+    }
+
+    /**
+     * returns current route path
+     */
+    public static function getRoutePath() {
+        $uri = self::getRequestURI();
+        $dir = preg_quote(self::getScriptDirectory(), "/");
+
+        return preg_replace(["/$dir/", "/\/+/"], ["", "/"], $uri);
     }
 }
