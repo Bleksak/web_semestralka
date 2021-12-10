5ce2048203f3229754d90e732a634dd93701f7ac
Jiří Velek <jiri.velek7@protonmail.com>
2021-12-10 22:29:24 +0100

    delete file upload from index

--- index.php
+++ index.php
@@ -5,21 +5,3 @@ require_once "Config.php";
 \helper\Session::start();
 
 $app = new \core\Application();
-
-// $user = new \model\User();
-
-// $user->login("asdf@asdf.com", "asd");
-// $user->register("asdf@asdf.com", "Jiri", "Velek", "asd");
-
-// if (Request::getRequestMethod() == "POST") {
-//     if (File::upload("file", "pdf")) {
-//         echo "ok";
-//     }
-// }
-
-
-// $loader = new \Twig\Loader\FilesystemLoader("view");
-// $twig = new \Twig\Environment($loader);
-
-// $view = $twig->load("upload.twig");
-// $view->display();
