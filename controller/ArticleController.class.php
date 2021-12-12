66f0c0c1bea317c03a2640e6d4e4bbbd881a6887
Jiří Velek <jvelek@students.zcu.cz>
2021-12-12 12:57:06 +0100

    check whether user is admin to view article

--- controller/ArticleController.class.php
+++ controller/ArticleController.class.php
@@ -3,6 +3,7 @@
 namespace controller;
 
 use model\Article;
+use model\User;
 use RuntimeException;
 
 class ArticleController extends Controller
@@ -15,9 +16,9 @@ class ArticleController extends Controller
             try {
                 $model = new Article();
                 $article = $model->get($params[0]);
-                if ($article !== false) {
-                    $this->setTitle($article->title);
+                if ($article !== false && ($article->approved || User::isAdmin() || User::getData()[User::SESSION_ID] == $article->author)) {
                     $article->date = strftime("%d. %B %G", strtotime($article->date));
+                    $this->setTitle($article->title);
                     $this->add("article", $article);
                 }
             } catch (RuntimeException $e) {
